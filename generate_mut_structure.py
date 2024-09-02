da2bea111b31ce155a1bba61d0343bd44cc9fe4f
Liu Yang <2739977040@qq.com>
2024-09-02 16:37:28 +0800

    Update generate_mut_structure.py

--- generate_mut_structure.py
+++ generate_mut_structure.py
@@ -15,29 +15,23 @@ def get_mutfile_txt(pdb1,mut1,chain1,path1):
 i = 0
 while i < len(sys.argv):
     if sys.argv[i] == "-w":
-        # 工作路径
         workdir = sys.argv[i + 1]
-        i += 2  # 跳过当前参数及其值
+        i += 2
     elif sys.argv[i] == "-p":
-        # 要处理的文件地址
         file = sys.argv[i + 1]
-        i += 2  # 跳过当前参数及其值 
+        i += 2 
     elif sys.argv[i] == "-m":
-        # 要处理的文件地址
         mut_chain = sys.argv[i + 1]
-        i += 2  # 跳过当前参数及其值 
+        i += 2 
     else:
-        # 如果当前参数不是我们想要的参数，则继续检查下一个参数
         i += 1
 
 os.chdir(workdir)
 chain = mut_chain.split('_')[1]
 mut = mut_chain.split('_')[0]
 
-# 生成突变信息文件
 get_mutfile_txt(file.split('.')[0],mut,chain,workdir)
 
-# 运行foldx，产生突变体结构
 os.system('./foldx --command=BuildModel --pdb='+file.split('.')[0]+'.pdb --mutant-file=individual_list_'+file.split('.')[0]+'_'+chain+'_1_mut.txt')
 
 mutdat = pd.read_csv(workdir + '/individual_list_'+file.split('.')[0]+'_'+chain+'_1_mut.txt',header=None,sep='\t').rename(columns={0:'mut'})
