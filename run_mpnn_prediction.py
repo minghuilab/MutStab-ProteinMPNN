f0f152ee6cc4b9dd905b73b113d8f016495d2282
Liu Yang <2739977040@qq.com>
2024-09-02 16:42:20 +0800

    Update run_mpnn_prediction.py

--- run_mpnn_prediction.py
+++ run_mpnn_prediction.py
@@ -22,7 +22,6 @@ from torch.cuda.amp import autocast, GradScaler
 from tqdm import tqdm
 from torch.optim.lr_scheduler import StepLR
 from sklearn.model_selection import train_test_split
-import torch.utils.data.dataset as Dataset
 from typing import List
 from pathlib import Path
 import yaml
@@ -43,8 +42,6 @@ mut = mut_chain.split('_')[0]
 fea_path = args.featurePath
 output_path = args.output
 
-
-# 对突变位点进行clean
 have_line = []
 with open(pdb_file_path,'r') as f:
     for line1 in f:
@@ -59,7 +56,6 @@ pos = mut[1:-1]
 newpos = pdb_dat[pdb_dat['Pos']==pos].reset_index(drop=True)['clean_Pos'][0]
 clean_mut = mut[0] + str(newpos) + mut[-1]
 
-# 提取ProteinMPNN的embedding特征
 feafile = dict(np.load(fea_path + pdb_file.split('.')[0] + '.npz'))
 ref_hid_emb = feafile['hid'].reshape(1,feafile['hid'].shape[1],-1)
 ref_embedding = np.squeeze(np.concatenate((feafile['embedding'],ref_hid_emb),axis=-1))
@@ -69,7 +65,6 @@ mut_hid_emb = mutfile['hid'].reshape(1,mutfile['hid'].shape[1],-1)
 mut_embedding = np.squeeze(np.concatenate((mutfile['embedding'],mut_hid_emb),axis=-1))
 
 def pad_three(arr,p,q,numi):
-    # 定义需要进行填充的形状 1D
     pad_shape0 = np.zeros((p,numi))
     pad_shape1 = np.zeros((q,numi))
     arr1 = np.concatenate((pad_shape0,arr,pad_shape1),axis=0)
@@ -84,8 +79,6 @@ def get_need_refmut_three(arr,mutpos,need_len,p,q,numi):
         return arr[mutpos-need_len:mutpos+need_len + 1,:]
     else:
         return pad_three(arr[mutpos - need_len:,:],p,q,numi)
-
-
 
 
 if int(newpos) <= 11:
@@ -105,82 +98,47 @@ mut_embedding_need = get_need_refmut_three(mut_embedding,int(newpos),11,pad_p,pa
 
 # module
 class MultiHeadSelfAttention(nn.Module):
-    # 类的构造方法
     def __init__(self, embed_dim, num_heads=8):
-        #继承自MultiHeadSelfAttention父类
         super(MultiHeadSelfAttention, self).__init__()
-        # 输入embedding特征的维度(每个残基的embedding特征的维度)
         self.embed_dim = embed_dim
-        # 多头注意力的头数heads
         self.num_heads = num_heads
-        # 注意：embedding的维度，应该能被heads整除？？？？？？？？？？？？？？？？？？？？？？？？？？？
         if embed_dim % num_heads != 0:
             raise ValueError(
                 f"embedding dimension = {embed_dim} should be divisible by number of heads = {num_heads}"
             )
-        # 初始化self.projection_dim？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
         self.projection_dim = embed_dim // num_heads
-        #构建计算q、k、v的线性层Wq、Wk、Wv
         self.query_dense = nn.Linear(embed_dim, embed_dim)
         self.key_dense = nn.Linear(embed_dim, embed_dim)
         self.value_dense = nn.Linear(embed_dim, embed_dim)
-        # 线性变化层：用于将多头注意力的输出合并成最终输出？？？？？？？？？？？？？？？？？？？？？
         self.combine_heads = nn.Linear(embed_dim, embed_dim)
 
-    # 注意力attention计算方法，即上面图片中的公式过程
     def attention(self, query, key, value, mask=None):
-        # torch.matmul执行矩阵乘法，计算自注意力分数矩阵，用于衡量每个位置的权重
-        # transpose(-2, -1)将key张量的最后两个维度进行调换，相当于转置T
         score = torch.matmul(query, key.transpose(-2, -1))
-        # 计算张量最后一个维度的大小（每个残基的特征维度）
-        # key.shape[-1]表示key张量，的特征维度（每个残基的特征维度），将最后一个特征维度保存为torch.float32的形式
         dim_key = torch.tensor(key.shape[-1], dtype=torch.float32)
-        # 在计算自注意力机制中，对注意力分数进行缩放，目的是控制注意力分数的范围，使其更加稳定，方便训练
         scaled_score = score / torch.sqrt(dim_key)
-        # 在自注意力机制中，应用掩码操作（mask），在某些位置上抑制或屏蔽注意力
-        # mask：一个与注意力分数矩阵相同形状的二元掩码矩阵，1表示屏蔽，0表示保留？？？？？？？？？？？？？？？
-        # scaled_score：经过缩放后的原始注意力分数矩阵
         if mask is not None:
             scaled_score += (mask * -1e9)
-        # 对缩放后的注意力分数，进行softmax操作，以计算注意力权重
-        # 再将softmax后的权重矩阵，与value（v）做矩阵乘法
         weights = F.softmax(scaled_score, dim=-1)
         output = torch.matmul(weights, value)
-        # 最终输出注意力分数Z和权重矩阵
         return output, weights
-    
-    # 将输入张量重塑形状，以便在多头注意力机制中分离不同的注意力头（在多头注意力中，输入张量通常被分成多个部分，每个部分对应一个注意力头）
-    # view：将x张量，重塑为其他形状，但是要求新形状的元素总数和原张量相同
-    # 交换张量的维度，使得到的最终形状为(0, 2, 1, 3) = (batch_size, num_heads, seq_len, projection_dim)
     def separate_heads(self, x, batch_size):
         x = x.view(batch_size, -1, self.num_heads, self.projection_dim)
         return x.permute(0, 2, 1, 3)
 
-    # forward函数：运行多头注意力的过程
-    # 输入：inputs是输入张量，mask掩码，默认是没有掩码None
     def forward(self, inputs, mask=None):
-        # x.shape = [batch_size, seq_len, embedding_dim]
-        #获取输入张量的批次batch_size，即这一批有多少个蛋白质（多少个序列）
         batch_size = inputs.size(0)
-        #计算q、k、v
         query = self.query_dense(inputs)  # (batch_size, seq_len, embed_dim)
         key = self.key_dense(inputs)  # (batch_size, seq_len, embed_dim)
         value = self.value_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        # 所有蛋白质的q在同一个张量中，此步骤将不同蛋白质的q、k、v分离出来
         query = self.separate_heads(query, batch_size)  # (batch_size, num_heads, seq_len, projection_dim)
         key = self.separate_heads(key, batch_size)  # (batch_size, num_heads, seq_len, projection_dim)
         value = self.separate_heads(value, batch_size)  # (batch_size, num_heads, seq_len, projection_dim)
-        # 计算多头注意力的结果，和权重矩阵
         attention, weights = self.attention(query, key, value, mask)
-        # 对得到的结果矩阵的张量，进行重塑
         attention = attention.permute(0, 2, 1, 3)  # (batch_size, seq_len, num_heads, projection_dim)
-        # 对多头注意力得到的结果进行reshape，再将reshape后的多头注意力concat在一起
-        # concat后得到的输出结果，就是多头自注意力的最终输出（维度不变）
         concat_attention = attention.reshape(batch_size, -1, self.embed_dim)  # (batch_size, seq_len, embed_dim)
         output = self.combine_heads(concat_attention)  # (batch_size, seq_len, embed_dim)
         return output
 
-    # 为自定义的 Keras 层提供配置信息，使得在保存和加载模型时，能够保存和恢复该层的参数。
     def get_config(self):
         config = super().get_config().copy()
         config.update({
@@ -190,14 +148,8 @@ class MultiHeadSelfAttention(nn.Module):
 
 class transformer_block(nn.Module):
     def __init__(self, input_dim,output_dim, head,dropout_rate):
-        # 调用父类cnn1d_block
         super(transformer_block, self).__init__()
-        # 定义一个多头注意力的类class，只需输入两个参数
-        # 输入特征的总维度（每个头的维度dim1* 头数head）
-        # head：多头注意力的头数，可以通过设置head的有无，选择是否加入多头注意力模块
         self.attention1 = MultiHeadSelfAttention(input_dim,head) if head is not None else None     
-        # 层归一化layerNorm
-        # eps=1e-6 : 为了稳定性
         self.layer_norm1 = nn.LayerNorm(normalized_shape=input_dim , eps = 1e-06)
         self.layer_norm2 = nn.LayerNorm(normalized_shape=input_dim , eps = 1e-06)
         self.fnn = nn.Sequential(nn.Linear(input_dim, output_dim),nn.ReLU(),nn.Linear(output_dim, output_dim))
@@ -255,8 +207,6 @@ class cnn1d_Residual_block(nn.Module):
         x3 = self.leakyReLu1(x2)
         x4 = self.conv2(x3.float())
         x5 = x4.permute(0, 2, 1)
-        # 调整x的维度使得与x5匹配
-#         x_adjusted = x.permute(0, 2, 1)
         x6 = torch.add(x, x5)
         x7 = self.leakyReLu1(x6)
         x8 = self.maxpooling1(x7)
@@ -273,7 +223,7 @@ class MLPs_block(nn.Module):
         self.bn2 = nn.BatchNorm1d(self.embed_dim // 8)
         self.fc3 = nn.Linear(self.embed_dim // 8, 1)
         self.leakyReLU = nn.LeakyReLU(negative_slope=0.2)
-        self.sigmoid = nn.Sigmoid()  # 使用LogSoftmax作为输出层的激活函数，方便计算交叉熵损失
+        self.sigmoid = nn.Sigmoid()
     
     def forward(self, x):
         out = self.fc1(x)
@@ -289,34 +239,17 @@ class MLPs_block(nn.Module):
 class PremPS2(nn.Module):
     def __init__(self, input_dim_neigh = 512):
         super().__init__() 
-        ## 邻居通道需要：
-        # Transformer模块：Multi-head attention + feed forward
-        # 计算添加positional encoding
         self.bn0 = nn.BatchNorm1d(input_dim_neigh)
         self.PE_neigh = TokenPositionEmbedding(23,pos_embed_dim = input_dim_neigh)
         self.transb1 = transformer_block(input_dim_neigh,input_dim_neigh,head = 2,dropout_rate=0.1)
         self.transb2 = transformer_block(input_dim_neigh,input_dim_neigh,head= 2,dropout_rate=0.1)
-        # 定义残差连接模块
         relu_par0 = 0.1
         self.cnnb1 = cnn1d_Residual_block(23,23,23,relu_par0, pool_kernel_size = 23)
-        # 定义最终输出模块
         self.in2_channel  = input_dim_neigh 
         self.mlps = MLPs_block(self.in2_channel)
 
     def get_attn_pad_mask(self, seq_x):
-        """
-        Generate attention pad mask based on input sequence.
-        Args:
-            seq_x (torch.Tensor): Input sequence tensor of shape [batch_size, seq_length, feature_dim].
-
-        Returns:
-            torch.Tensor: Attention pad mask tensor of shape [batch_size, 1, seq_length], where 0 indicates a padding position
-                            and 1 indicates a non-padding position.
-        """
-        # Extract the feature dimension from the sequence
-#         seq_q = seq_x.unsqueeze(1)
         feature_dim = seq_x.size(-1)
-        # Check if the feature dimension is all zeros
         pad_mask = (seq_x.sum(dim=-1) == 0).unsqueeze(1)
         return pad_mask
 
@@ -327,9 +260,6 @@ class PremPS2(nn.Module):
         mut_neigh = self.bn0(mut_neigh.permute(0, 2, 1))
         wt_neigh = wt_neigh.permute(0, 2, 1)
         mut_neigh = mut_neigh.permute(0, 2, 1)
-        # print(mask_wt.shape)
-        ## 邻居通道
-        # 加PE
         wt_neigh = self.PE_neigh(wt_neigh)
         mut_neigh = self.PE_neigh(mut_neigh)
         # Transformer
@@ -353,7 +283,6 @@ mutsta_mpnn.load_state_dict(mutsta_mpnn_weights)
 
 mutsta_mpnn.eval()
 
-# predict
 torch_ref_embedding_need = torch.from_numpy(ref_embedding_need).unsqueeze(0)
 torch_mut_embedding_need = torch.from_numpy(mut_embedding_need).unsqueeze(0)
 
